b28bbce8aa5a31a7754dee82827b6334381cd374
barahouei <abdulghanibarahouei@gmail.com>
2023-04-23 12:28:42 +0330

    feat: solve twosum with map method

--- twosum/twosum.go
+++ twosum/twosum.go
@@ -3,12 +3,14 @@ package twosum
 func TwoSum(nums []int, target int) []int {
 	res := []int{}
 
-	for i, left := range nums {
-		for j, right := range nums {
-			if left+right == target && i != j {
-				res = append(res, i)
-			}
+	m := make(map[int]int)
+
+	for i, num := range nums {
+		if _, ok := m[num]; ok {
+			res = append(res, m[num], i)
 		}
+
+		m[target-num] = i
 	}
 
 	return res
