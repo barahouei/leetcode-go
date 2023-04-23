3ab71d9f8537ce6cee3e241407592591016b63ee
barahouei <abdulghanibarahouei@gmail.com>
2023-04-23 12:23:44 +0330

    feat: solve twosum with bruteforce method

--- twosum/twosum.go
+++ twosum/twosum.go
@@ -3,5 +3,13 @@ package twosum
 func TwoSum(nums []int, target int) []int {
 	res := []int{}
 
+	for i, left := range nums {
+		for j, right := range nums {
+			if left+right == target && i != j {
+				res = append(res, i)
+			}
+		}
+	}
+
 	return res
 }
