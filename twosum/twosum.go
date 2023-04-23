5ed5a4c0936b2d48439e410bc77b7c4053b1006d
barahouei <abdulghanibarahouei@gmail.com>
2023-04-23 13:35:47 +0330

    feat: add comment to twosum problem

--- twosum/twosum.go
+++ twosum/twosum.go
@@ -1,5 +1,7 @@
+// Package twosum shows sum of which numbers is equal to the target.
 package twosum
 
+// TwoSum returns a slice of indices that sum of them matches the target.
 func TwoSum(nums []int, target int) []int {
 	res := []int{}
 
