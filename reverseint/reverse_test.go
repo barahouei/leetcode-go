236da4e4f78e75605e9f99a1144d26f9b8ea6a44
barahouei <abdulghanibarahouei@gmail.com>
2023-04-30 16:55:20 +0330

    test: add test for reverse integer problem

--- reverseint/reverse_test.go
+++ reverseint/reverse_test.go
@@ -1,1 +1,37 @@
 package reverseint
+
+import "testing"
+
+var testCases = []struct {
+	desc     string
+	input    int
+	expected int
+}{
+	{
+		desc:     "test1",
+		input:    123,
+		expected: 321,
+	},
+	{
+		desc:     "test2",
+		input:    -123,
+		expected: -321,
+	},
+	{
+		desc:     "test3",
+		input:    120,
+		expected: 21,
+	},
+}
+
+func TestReverse(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			got := Reverse(tc.input)
+
+			if got != tc.expected {
+				t.Errorf("got: %d, want: %d", got, tc.expected)
+			}
+		})
+	}
+}
